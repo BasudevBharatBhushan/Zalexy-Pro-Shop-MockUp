ca5a23670cf71e00feb69a7f7f68abe95d398003
Basudev Bharat Bhushan <bharatbhushan173@gmail.com>
2026-05-14 17:10:24 +0000

    added primsa

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.product.createMany({
+    data: [
+      {
+        name: "iPhone 15",
+        price: 999,
+      },
+      {
+        name: "Samsung S24",
+        price: 899,
+      },
+    ],
+  });
+
+  console.log("Seeded successfully");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
